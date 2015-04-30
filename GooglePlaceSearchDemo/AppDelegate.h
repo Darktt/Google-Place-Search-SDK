e2d4eda43200f3b455b374c33ee3761703d9b63d
Eden Li <eden@realtime.tw>
2015-04-30 10:36:38 +0800

    Initial Commit

---
+++ GooglePlaceSearchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GooglePlaceSearchDemo
+//
+//  Created by EdenLi on 2015/4/30.
+//  Copyright (c) 2015年 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GooglePlaceSearchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GooglePlaceSearchDemo
+//
+//  Created by EdenLi on 2015/4/30.
+//  Copyright (c) 2015年 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
